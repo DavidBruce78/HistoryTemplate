6cf338cba051d54c0837325ed8869791f0eaf361
DavidBruce78 <DavidBruce78@gmail.com>
2017-03-03 13:48:03 -0500

    Added all files for website

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>History Website Template</title>
+    <!-- Bootstrap -->
+    <link href="https://fonts.googleapis.com/css?family=Droid+Serif" rel="stylesheet">
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link href="css/historyCss.css" rel="stylesheet">
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="container" id="wrapper">
+      <div class="row">
+          <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 frame1">
+            <img src="images/oldpaperheader.png" class="img-responsive headerfooterimage">
+          </div>
+      </div>
+      <div class="row">
+          <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12 frame2">
+            <h3>History Lessons</h3>
+            <img src="images/historyquizimg.png" class="col-lg-4 col-md-4 col-sm-4 col-xs-4 img-responsive imgrow1">
+            <img src="images/ourhistoryimg.jpg" class="col-lg-4 col-md-4 col-sm-4 col-xs-4 img-responsive imgrow1">
+            <img src="images/presidentsimg.jpg" class="col-lg-4 col-md-4 col-sm-4 col-xs-4 img-responsive imgrow1">
+            <form id="searchform"style="color:white;">
+            <label for="searchfield">SEARCH HISTORY DATABASE</label><br/>
+            <input type="text" name="searchfield" Value="Enter Search" style="color:black;"/>
+            <input type="submit" name="search" value="Search" style="background-color: #952a08;"/>
+            </form>
+            <hr class="hidden-sm hidden-xs " style="color:#821d0b"/>
+          </div>
+          <div class="col-lg-4 col-md-4 hidden-sm hidden-xs frame3">
+            <h2>Today In History</h2>
+            <p>Today in 1945 History was made
+            when all of America was thrusted into
+            a major war with the world!
+            Hello world!fakfp'dv'sdvsdvksdvs
+            vsdvsdvsdvsldvmslvmsldvmslvmsvmsvl
+            vlsdmvsldvmkslkvmsvmslvmklsvmsvmvksm
+            Hello world!fakfp'dv'sdvsdvksdvs
+            vsdvsdvsdvsldvmslvmsldvmslvmsvmsvl
+            vlsdmvsldvmkslkvmsvmslvmklsvmsvmvksm
+            </p>
+          </div>
+      </div>
+      <div class="row">
+        <hr class="hidden-md hidden-lg">
+      </div>
+      <div class="row">
+        <div class="hidden-lg hidden-md col-sm-12 col-xs-12 todayinhis">
+          <h2>Today In History</h2>
+          <p>Today in 1945 History was made
+            when all of America was thrusted 
+            into a major war with the world!
+            Hello world!fakfp'dv'sdvsdvksdvs
+          </p>
+        </div>
+      </div>
+      <div class="row">
+        <img class="img-responsive hidden-lg hidden-md col-sm-12 col-xs-12" style="box-shadow: 1px 1px 5px #821d0b;" src="images/ww2img.jpg">
+        
+      </div>
+      <div class="row">
+        <div class="col-lg-8 col-md-8" style="text-align:center;">
+         <h2>Pyramids Of The God's</h2>
+         <img class="img-responsive" style="box-shadow: 1px 1px 5px #821d0b;" src="images/pyramids1.jpg"><br/>
+         <p>Far far away, behind the word mountains, 
+          far from the countries Vokalia and Consonantia, 
+          there live the blind texts. Separated they live in Bookmarksgrove right
+           at the coast of the Semantics, a large language ocean. 
+           A small river named Duden flows by their place and supplies it with the
+            necessary regelialia. It is a paradisematic country, 
+            in which roasted parts of sentences fly into your mouth. 
+            rld of Grammar.
+         </p>
+        </div>
+        <div class="col-lg-4 col-md-3 hidden-sm hidden-xs frame4">
+          <h2>Top 10 History Books</h2>
+          <p>We have provided you with a list of history's
+             top history books. We have scoured the History
+             archives and used the expertise of historical authors
+             to help us pick the most riveting and authoritative titles.
+          </p>
+          <img class="img-responsive" style="box-shadow: 1px 1px 10px #821d0b;" src="images/oldbooksimg.jpg">
+        </div> 
+
+      </div>
+     <footer> <img src="images/footer.jpg" class="img-responsive headerfooterimage">
+     </footer>
+    </div>
+ 
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
